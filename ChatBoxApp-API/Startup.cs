29c22409b85e434de158bc75a333e827becd40f7
Ashan Madushanka <ashan_m@3logixs.lk>
2020-06-15 14:25:19 +0530

    basic Cors allow setup

--- ChatBoxApp-API/Startup.cs
+++ ChatBoxApp-API/Startup.cs
@@ -30,6 +30,7 @@ namespace ChatBox_API
             services.AddDbContext<DataContext>(x => x.UseSqlite
             (Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllers();
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -43,6 +44,8 @@ namespace ChatBox_API
             // app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
 
             app.UseAuthorization();
 
