31d15423e75601371b5fc37636dc7d0c24bb3e1b
Ashan Madushanka <ashan_m@3logixs.lk>
2020-07-09 18:49:17 +0530

    login method completed with JWT token

--- ChatBoxApp-API/Controllers/AuthController.cs
+++ ChatBoxApp-API/Controllers/AuthController.cs
@@ -1,8 +1,14 @@
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 using ChatBoxApp_API.Data;
 using ChatBoxApp_API.Dtos;
 using ChatBoxApp_API.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 
 namespace ChatBoxApp_API.Controllers
 {
@@ -11,9 +17,11 @@ namespace ChatBoxApp_API.Controllers
     public class AuthController : ControllerBase
     {
         private readonly IAuthRepository _authRepository;
+        private readonly IConfiguration _config;
 
-        public AuthController(IAuthRepository authRepository)
+        public AuthController(IAuthRepository authRepository, IConfiguration config)
         {
+            this._config = config;
             this._authRepository = authRepository;
         }
 
@@ -27,7 +35,8 @@ namespace ChatBoxApp_API.Controllers
                 return BadRequest("Username already exists");
             }
 
-            var userToCreate = new User {
+            var userToCreate = new User
+            {
                 Username = userForRegisterDto.Username
             };
 
@@ -37,16 +46,38 @@ namespace ChatBoxApp_API.Controllers
         }
 
         [HttpPost("login")]
-        public async Task<IActionResult> Login(UserForLoginDto userForRegisterDto)
+        public async Task<IActionResult> Login(UserForLoginDto userForLoginDto)
         {
-            var userFromRepo = await _repo.Login(userForRegisterDto.Username, userForRegisterDto.Password);
+            var userFromRepo = await _authRepository.Login(userForLoginDto.Username.ToLower(), userForLoginDto.Password);
 
             if (userFromRepo == null)
             {
                 return Unauthorized();
             }
 
-            
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, userFromRepo.Id.ToString()),
+                new Claim(ClaimTypes.Name, userFromRepo.Username)
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.
+                        GetBytes(_config.GetSection("AppSettings:Token").Value));
+                    
+            var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(1),
+                SigningCredentials = credentials
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return Ok(new { token = tokenHandler.WriteToken(token)});
         }
     }
 }
