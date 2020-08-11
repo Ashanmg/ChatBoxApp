e3cbe9c4e4617c49f16e824038958797a9ad6b15
Ashan Madushanka <ashan_m@3logixs.lk>
2020-08-11 23:24:22 +0530

    basic error handling for global added

--- ChatBoxApp-API/Startup.cs
+++ ChatBoxApp-API/Startup.cs
@@ -56,6 +56,20 @@ namespace ChatBox_API
             {
                 app.UseDeveloperExceptionPage();
             }
+            else
+            {
+                app.UseExceptionHandler(builder =>{
+                    builder.Run(async context =>{
+                        context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+                        var error = context.Features.Get<IExceptionHandlerFeature>();
+                        if (error !== null)
+                        {
+                            await context.Response.WriteAsync(error.Error.Message);
+                        }
+                    });
+                });
+            }
 
             // app.UseHttpsRedirection();
 
