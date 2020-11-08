d63e52529a823d05493384f3e886761e9e382637
Ashan Madushanka <ashan_m@3logixs.lk>
2020-11-08 10:40:47 +0530

    change the allow and disallow url to the production

--- ChatBoxApp-SPA/src/environments/environment.prod.ts
+++ ChatBoxApp-SPA/src/environments/environment.prod.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://chatboxonline-api.azurewebsites.net/api/'
+  apiUrl: 'https://chatboxonline-api.azurewebsites.net/api/',
+  allowDomain: 'chatboxonline-api.azurewebsites.net',
+  disallowDomain: 'chatboxonline-api.azurewebsites.net/api/auth'
 };
--- ChatBoxApp-SPA/src/environments/environment.ts
+++ ChatBoxApp-SPA/src/environments/environment.ts
@@ -4,7 +4,9 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:5000/api/'
+  apiUrl: 'http://localhost:5000/api/',
+  allowDomain: 'localhost:5000',
+  disallowDomain: 'localhost:5000/api/auth'
 };
 
 /*
