2424662f7050ffaaddaa87dd5bd0447065f5261b
Kevin Fernando <kevin.fernando@nzsafetyblackwoods.co.nz>
2020-12-17 22:57:59 +0530

    Update main photo option API created

--- ChatBoxApp-API/Data/DatingRepository.cs
+++ ChatBoxApp-API/Data/DatingRepository.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using ChatBox_API.Data;
 using ChatBoxApp_API.Models;
@@ -28,6 +29,11 @@ namespace ChatBoxApp_API.Data
             var photo =  await _context.Photos.FirstOrDefaultAsync(p => p.Id == id);
 
             return photo;
+        }
+        
+        public async Task<Photo> GetMainPhotoForUser(int userId)
+        {
+            return await _context.Photos.Where(u => u.UserId == userId).FirstOrDefaultAsync( p => p.IsMain);
         }
 
         public async Task<User> GetUser(int id)
--- ChatBoxApp-API/Data/IDatingRepository.cs
+++ ChatBoxApp-API/Data/IDatingRepository.cs
@@ -6,11 +6,12 @@ namespace ChatBoxApp_API.Data
 {
     public interface IDatingRepository
     {
-         void Add<T>(T entity) where T: class;
-         void Delete<T>(T entity) where T: class;
-         Task<bool> SaveAll();
-         Task<IEnumerable<User>> GetUsers();
-         Task<User> GetUser(int id);
-         Task<Photo> GetPhoto(int id);
+        void Add<T>(T entity) where T: class;
+        void Delete<T>(T entity) where T: class;
+        Task<bool> SaveAll();
+        Task<IEnumerable<User>> GetUsers();
+        Task<User> GetUser(int id);
+        Task<Photo> GetPhoto(int id);
+        Task<Photo> GetMainPhotoForUser(int userId);
     }
 }
